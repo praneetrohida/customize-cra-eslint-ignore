57d1725a58094797d4954eac9b963532a4c286e2
Praneet Rohida <praneetrohida@gmail.com>
2019-08-30 13:37:21 +0530

    First commit

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+const enableEslintIgnore = () => (config: Record<string, any>) => {
+  const eslintRule = config.module.rules.filter(
+    (r: Record<string, any>) =>
+      r.use &&
+      r.use.some(
+        (u: Record<string, any>) =>
+          u.options && u.options.useEslintrc !== void 0
+      )
+  )[0];
+
+  eslintRule.use[0].options.ignore = true;
+
+  const rules = config.module.rules.map((r: Record<string, any>) =>
+    r.use &&
+    r.use.some(
+      (u: Record<string, any>) => u.options && u.options.useEslintrc !== void 0
+    )
+      ? eslintRule
+      : r
+  );
+  config.module.rules = rules;
+  return config;
+};
+
+export default enableEslintIgnore;
